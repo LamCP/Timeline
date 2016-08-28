fa65b89ff263bf5dff36da6536f76f9898ed1bad
Lin Ma <malin@xinzhili.cn>
2016-08-28 22:17:25 +0800

    fix:orm bug

--- app/src/main/java/com/maic/timeline/timeline/MainActivity.java
+++ app/src/main/java/com/maic/timeline/timeline/MainActivity.java
@@ -9,6 +9,7 @@ import com.orm.SugarContext;
 import com.orm.SugarRecord;
 
 import java.util.Date;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Plan plan = new Plan("adasd",new Date());
+
+        Plan plan = new Plan("test", new Date());
         plan.save();
 
-        Plan plan1 = Plan.findById(Plan.class,1);
-        Log.e("orm",plan1.planName);
+        Plan plan1 = Plan.findById(Plan.class, 1);
+        Log.e("orm", plan1.planName);
+        Iterator<Plan> planIterator = Plan.findAll(Plan.class);
+        while (planIterator.hasNext()){
 
-
-
+        }
+        
     }
 }
